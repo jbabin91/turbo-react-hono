f629cdb81c50f71352399464e78ed52435c56ed5
Jace Babin <jbabin91@gmail.com>
2024-10-07 11:56:57 -0700

    build(web): :package: add path alias

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,7 +1,35 @@
+import { fileURLToPath } from 'node:url';
+
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name].[hash].[ext]',
+        chunkFileNames: 'assets/[name].[hash].js',
+        entryFileNames: 'assets/[name].[hash].js',
+        manualChunks: {
+          react: ['react', 'react-dom'],
+        },
+      },
+    },
+  },
   plugins: [react()],
+  preview: {
+    port: 5173,
+  },
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: fileURLToPath(new URL('src', import.meta.url)),
+      },
+    ],
+  },
+  server: {
+    port: 5173,
+  },
 });
