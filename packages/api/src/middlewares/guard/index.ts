06ab357b3bf191e822cf264d1ce2127801e9b393
Jace Babin <jbabin91@gmail.com>
2024-10-12 16:50:38 -0700

    feat(api): :sparkles: update system admin check to use middleware factory

--- packages/api/src/middlewares/guard/index.ts
+++ packages/api/src/middlewares/guard/index.ts
@@ -1,4 +1,3 @@
-import { type Context, type Next } from 'hono';
 import { every } from 'hono/combine';
 import { createMiddleware } from 'hono/factory';
 
@@ -6,20 +5,17 @@ import { getUserContext } from '../../lib/context';
 import { errorResponse } from '../../lib/errors';
 
 // System admin is a user with the 'admin' role in the users table.
-export async function isSystemAdmin(
-  ctx: Context,
-  next: Next,
-): Promise<Response | undefined> {
+export const isSystemAdmin = createMiddleware(async (c, next) => {
   const user = getUserContext();
 
   const isSystemAdmin = user?.role.includes('admin');
   if (!isSystemAdmin)
-    return errorResponse(ctx, 403, 'warn', {
+    return errorResponse(c, 403, 'warn', {
       user: user.id,
     });
 
   await next();
-}
+});
 
 // Combine system admin check with IP restriction.
 export const systemGuard = every(
