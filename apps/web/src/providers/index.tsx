ca09b374c56b7916f0f45d1608892a5d5994bc60
Jace Babin <jbabin91@gmail.com>
2024-10-12 16:53:17 -0700

    feat(web): :sparkles: add tanstack query provider

--- apps/web/src/providers/index.tsx
+++ apps/web/src/providers/index.tsx
@@ -1,5 +1,11 @@
 import { ThemeProvider } from '@repo/ui';
 
+import { TanstackQueryProvider } from './tanstack-query-provider';
+
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <ThemeProvider>{children}</ThemeProvider>;
+  return (
+    <ThemeProvider>
+      <TanstackQueryProvider>{children}</TanstackQueryProvider>
+    </ThemeProvider>
+  );
 }
